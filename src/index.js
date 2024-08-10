6884c82dc0bff0d4e35e753afb79d99b83a6db13
Konstantin Petrov <43613041+petruse4ka@users.noreply.github.com>
2024-08-10 21:32:07 +0200

    feat: add solution to the task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    
+    for (let i = 0; i < expr.length; i += 10) {
+        arr = arr.concat(expr.slice(i, (10 + i)).split())
+    }
+
+    let arrFilter = []
+
+    for (let j = 0; j < arr.length; j++) {
+        arrFilter = arrFilter.concat(arr[j].replaceAll('00', '').replaceAll('10', '.').replaceAll('11', '-'))
+    }
+
+    let arrMorse = Object.keys(MORSE_TABLE)
+
+    for (let k = 0; k < arrMorse.length; k++) {
+        for (let m = 0; m < arrFilter.length; m++) {
+            if (arrFilter[m] === arrMorse[k]) {
+                arrFilter[m] = MORSE_TABLE[arrMorse[k]]
+            } else if (arrFilter[m] === '**********') {
+                arrFilter[m] = ' '
+            }
+        }
+    }
+
+    let result = arrFilter.join('');
+
+    return result;
 }
 
 module.exports = {
